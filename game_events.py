0123db97590ef4edd6daca0d662b42e8606d7385
Daniel Muckerman <danielmuckerman@me.com>
2020-11-07 15:23:08 -0500

    Remove unused Options event

--- game_events.py
+++ game_events.py
@@ -3,7 +3,6 @@ import pygame
 SCENE_REFOCUS = pygame.event.custom_type()
 NEW_GAME = pygame.event.custom_type()
 CONTINUE = pygame.event.custom_type()
-OPTIONS = pygame.event.custom_type()
 PAUSE_CONTINUE = pygame.event.custom_type()
 PAUSE_SAVE_AND_QUIT = pygame.event.custom_type()
 PAUSE_QUIT_TO_MENU = pygame.event.custom_type()
